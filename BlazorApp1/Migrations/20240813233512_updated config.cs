3e8d255976bc0eb06148c1839e25a6f9e3463b46
Mike Hacker <mhacker@microsoft.com>
2024-08-13 19:37:13 -0400

    Added optional abililty to auto approve requests

---
+++ BlazorApp1/Migrations/20240813233512_updated config.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAIChat.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedconfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutomaticAccountApproval",
+                table: "Config",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutomaticAccountApproval",
+                table: "Config");
+        }
+    }
+}
