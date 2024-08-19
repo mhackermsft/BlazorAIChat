e1a2ba59a1999e70c8dac05ec12f17d15df04323
Mike Hacker <61195478+mhackermsft@users.noreply.github.com>
2024-08-19 10:27:34 -0400

    Added document upload indicators in chat.

--- BlazorAIChat/Utils/FileUtils.cs
+++ BlazorAIChat/Utils/FileUtils.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace BlazorAIChat.Utils
 {
-    public static class ImageUtils
+    public static class FileUtils
     {
         public static string GetMimeTypeFromImage(Stream stream)
         {
@@ -22,8 +22,8 @@ namespace BlazorAIChat.Utils
 
             stream.Position = 0;
 
-            //if (bmp.SequenceEqual(buffer.Take(bmp.Length)))
-            //    return "image/bmp";
+            if (bmp.SequenceEqual(buffer.Take(bmp.Length)))
+                return "image/bmp";
 
             if (gif.SequenceEqual(buffer.Take(gif.Length)))
                 return "image/gif";
@@ -31,17 +31,39 @@ namespace BlazorAIChat.Utils
             if (png.SequenceEqual(buffer.Take(png.Length)))
                 return "image/png";
 
-            //if (tiff.SequenceEqual(buffer.Take(tiff.Length)))
-            //    return "image/tiff";
+            if (tiff.SequenceEqual(buffer.Take(tiff.Length)))
+                return "image/tiff";
 
-            //if (tiff2.SequenceEqual(buffer.Take(tiff2.Length)))
-            //    return "image/tiff";
+            if (tiff2.SequenceEqual(buffer.Take(tiff2.Length)))
+                return "image/tiff";
 
             if (jpeg.SequenceEqual(buffer.Take(jpeg.Length)))
                 return "image/jpeg";
 
             if (jpeg2.SequenceEqual(buffer.Take(jpeg2.Length)))
                 return "image/jpeg";
+
+            return string.Empty;
+        }
+
+        public static string GetIconForFileType(string filename)
+        {
+
+
+            if (filename.EndsWith(".pdf"))
+                return "/images/pdf_256x256.png";
+
+            if (filename.EndsWith(".docx"))
+                return "/images/word_256x256.png";
+
+            if (filename.EndsWith(".xlsx"))
+                return "/images/excel_256x256.png";
+
+            if (filename.EndsWith(".pptx"))
+                return "/images/powerpoint_256x256.png";
+
+            if (filename.EndsWith("txt"))
+                return "/images/txt_256x256.png";
 
             return string.Empty;
         }
